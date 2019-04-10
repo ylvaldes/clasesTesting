0b4fca56b12e9444b6597cda0276b92a387cd37c
Yasmani <yasman05@gmail.com>
2019-04-10 15:16:57 -0300

    Create:

--- Prueba.py
+++ Prueba.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python
 import os
-import subprocess
-import sys
-import datetime
-import os
-import shutil
-import fnmatch 
-import string
-import shutil
-import zipfile
+from pathlib import Path
+
+    
+new_dir='\.gemaTest'
+home= Path.home()
+if not os.path.exists(home+new_dir):
+        os.makedirs(new_dir)
 
 
-# Carpeta de gemadesa
-origen='D:\Yasmani\gemadesa\eclipse3.4-java\workspace'
-nombreClase='UTD_Capter'
-ext='.java'
-
-path_list = [os.path.join(root, file) for root, _, files in os.walk(origen)
-                                      for file in fnmatch.filter(files, nombreClase+ext)]
-print(path_list)
-
-rev_list=[]
-reader= os.popen('svn log -v --limit 1 '+path_list[0])
-
-for line in reader:
-    print(line)
-    if len(line.split('|'))== 4:
-        rev_list.append(line.split('|'))
-reader.close()
-print(rev_list)
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-clasesTesting
+1.	Inicialmente debemos configurar en el Scrip estas variables 
+#Carpeta Origen de los compilados
+origen='D:\gemadesa\IBM\MAXIMO'
+#Carpeta Usada para Guardar los Paquetes de Migracion
+carpeta_migracion='D:\Migracion'
+#Direccion del Codigo
+carpeta_codigo='D:\gemadesa\eclipse3.4-java\workspace'
+
+2.	En el archivo clases.txt ponemos el nombre de las clases que integran el paquete sin extensión. 
+3.	Al ejecutar el Scrip nos pedirá el número del parisnet y descripción del pasaje (Corto este es para identificar la carpeta)
+a.	Se crea la carpeta  YYYYMMDD_#Parisnet_Descripcion
+b.	Dentro de la carpeta:
+i.	Un txt con los detalles de la actualización (Parsnte, clases actualizada, ruta de las clases)
+ii.	La carpeta con las clases compilada y su ruta.
+iii.	El zip en el formato solicitado para el pasaje a Testing. 
